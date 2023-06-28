521ee242c728ef16718cccaa3d53ca39a1da4db4
Kristof Sotet <kristof.sotet@morrisonsplc.co.uk>
2023-06-28 11:18:17 +0100

    refactor: remove mysql connector

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,4 @@
 import express from 'express'
-import mysql from 'mysql2'
 
 import { config } from 'config'
 import { helloWorldRouter } from 'routes'
@@ -8,8 +7,6 @@ const { DATABASE_URL } = config
 
 export const runServer = () => {
   const app = express()
-  const db = mysql.createConnection(DATABASE_URL)
-  db.connect((err) => console.log(err))
 
   app.use(express.json())
   app.use('/helloworld', helloWorldRouter)
